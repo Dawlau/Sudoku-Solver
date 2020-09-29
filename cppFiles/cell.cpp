fa0d9fd26d1f2de5488866c6b521ab159622ad39
Dawlau <blahoviciandrei1@gmail.com>
2020-09-29 17:55:25 +0300

    Added a sf::RectangleShape field in cell class and gave it a functionality that renders the containing number

--- cppFiles/cell.cpp
+++ cppFiles/cell.cpp
@@ -1,33 +1,61 @@
 #include "cell.hpp"
 #include "SFML/Graphics.hpp"
+#include <iostream>
 #include "constants.hpp"
 
 
-cell::cell(const int &row, const int &col, const int &number){
+cell::cell(const int &row = 0, const int &col = 0, const int &number = 0)
+{
 
 	this->row = row;
 	this->col = col;
 	this->number = number;
-}
-
-void cell::draw(sf::RenderWindow &window) const{
 
 	sf::RectangleShape rect;
-	rect.setSize(sf::Vector2f(cellSize, cellSize));
-	rect.setOutlineColor(black);
+
+	rect.setSize(sf::Vector2f(pxCellSize, pxCellSize));
+	rect.setOutlineColor(sf::Color::Black);
 	rect.setOutlineThickness(1);
 
-	int pxGridSize = cellSize * gridSize;
+	int pxGridSize = pxCellSize * gridSize;
 
 	int XoffSet = width / 2 - pxGridSize / 2;
 	int YoffSet = height / 2 - pxGridSize / 2;
 
-	int x = row * cellSize + XoffSet;
-	int y = col * cellSize + YoffSet;
+	int x = row * pxCellSize + XoffSet;
+	int y = col * pxCellSize + YoffSet;
 
 	rect.setPosition(sf::Vector2f(x, y));
 
+	this->rect = rect;
+}
+
+
+void cell::drawInto(sf::RenderWindow &window){
 	window.draw(rect);
+
+	std::string no = std::to_string(this->number);
+
+	sf::Text text;
+	text.setFont(font);
+	text.setString(no);
+	text.setFillColor(sf::Color::Black);
+	text.setCharacterSize(pxTextSize);
+
+	sf::FloatRect prototype = rect.getGlobalBounds();
+	text.setPosition(sf::Vector2f(prototype.left + (pxCellSize - text.getGlobalBounds().width) / 2, prototype.top));
+
+
+	window.draw(text);
 }
 
 cell::~cell() {}
+
+
+int cell::getNumber() const{
+	return number;
+}
+
+void cell::setNumber(const int &number){
+	this->number = number;
+}
--- cppFiles/cell.hpp
+++ cppFiles/cell.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
 #include "SFML/Graphics.hpp"
+#include "constants.hpp"
 
 
-class cell
-{
+class cell{
+
 	private:
 		int row, col, number;
+		sf::RectangleShape rect;
 
 	public:
-
+		cell(){}
 		cell(const int &, const int &, const int &);
 		~cell();
-		void draw(sf::RenderWindow &) const;
+		void drawInto(sf::RenderWindow &);
+		int getNumber() const;
+		void setNumber(const int &);
 };
